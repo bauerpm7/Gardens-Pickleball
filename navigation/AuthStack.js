6463b0790d685dcc8f43652879f690027be77a39
Mike Bauer <mikeb@parsed.io>
2022-09-23 16:52:26 -0700

    add firebase-auth and styled components

---
+++ navigation/AuthStack.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+
+const Stack = createStackNavigator();
+
+export default function AuthStack() {
+  const screenOptions = {
+    headerShown: false
+  }
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen name='Login' component={LoginScreen} />
+      <Stack.Screen name='Signup' component={SignupScreen} />
+    </Stack.Navigator>
+  );
+}
